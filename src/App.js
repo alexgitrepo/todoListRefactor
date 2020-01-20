945de6f45d666dbee31d41c50f26fe00874475c6
alexgitrepo <alex.dev4web.com>
2020-01-20 15:40:23 +0500

    Made new functionality - add new task and so On.Done 3

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import TodoListFooter from "./TodoListFooter";
 
 class App extends React.Component {
     state = {
-        tasks: [{type: "checkbox", checked: true, taskName: "CSS",priority:"priority: Looow Yeah"}, {
+        tasks: [{type: "checkbox", checked: true, taskName: "CSS", priority: "priority: Looow Yeah"}, {
             type: "checkbox",
             checked: true,
             taskName: "JS",
-            priority:"priority: Very high"
-        }, {type: "checkbox", checked: true, taskName: "Patterns",priority:"priority: Medium"}],
-        filterValue:"ALL"
+            priority: "priority: Very high"
+        }, {type: "checkbox", checked: true, taskName: "Patterns", priority: "priority: Medium"}],
+        filterValue: "ALL"
     }
+    addNewTask = (newTaskTextFromInput) => { debugger
+        let newTask = {type: "checkbox", checked: true, taskName: newTaskTextFromInput, priority: "priority: Looow Yeah"}
+this.setState({...this.state, tasks:[...this.state.tasks,newTask]})
+    }
+
+
     render = () => {
         return (
             <div className="App">
                 <div className="todoList">
-                    <TodoListHeader/>
+                    <TodoListHeader addNewTask={this.addNewTask}/>
                     <TodoListTasks tasks={this.state.tasks}/>
                     <TodoListFooter filterValue={this.state.filterValue}/>
                 </div>
--- src/TodoListHeader.js
+++ src/TodoListHeader.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
 class TodoListHeader extends React.Component {
+    state={inputValue:''}
+    inputOnChange=(e)=>{
+        this.setState({inputValue:e.target.value})
+    }
+    addNewTask=(e)=>{debugger
+        this.props.addNewTask(this.state.inputValue)
+        this.setState({inputValue:""})
+    }
     render = () => {
         return (
             <div className="todoList-header">
                 <h3 className="todoList-header__title">What to Learn</h3>
                 <div className="todoList-newTaskForm">
-                    <input type="text" placeholder="New task name"/>
-                    <button>Add</button>
+                    <input onChange={this.inputOnChange} value={this.state.inputValue} type="text" placeholder="New task name"/>
+                    <button onClick={this.addNewTask}>Add</button>
                 </div>
             </div>
         );
