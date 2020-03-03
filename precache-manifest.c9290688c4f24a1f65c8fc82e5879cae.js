dcef9dcf97278f26b6f041972680365c9eec72f9
alexgitrepo <alex.dev4web.com>
2020-03-03 19:00:18 +0500

    Updates

---
+++ precache-manifest.c9290688c4f24a1f65c8fc82e5879cae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48d5bc64f131f93cd31af528d2d051a7",
+    "url": "/todoListRefactor/index.html"
+  },
+  {
+    "revision": "7a546e101774837169e7",
+    "url": "/todoListRefactor/static/css/main.4b93dd43.chunk.css"
+  },
+  {
+    "revision": "f802157d719623b066ac",
+    "url": "/todoListRefactor/static/js/2.13889153.chunk.js"
+  },
+  {
+    "revision": "7a546e101774837169e7",
+    "url": "/todoListRefactor/static/js/main.b089faa7.chunk.js"
+  },
+  {
+    "revision": "2eab71838652507270f7",
+    "url": "/todoListRefactor/static/js/runtime~main.fa58dfed.js"
+  }
+]);
